46319886731d9c560917fff6a4580231257290a8
Eresea <nils.c56@gmail.com>
2016-04-28 00:25:23 +0200

    Test fix

---
+++ matcreuse.h
@@ -0,0 +1,84 @@
+/**
+ * \file matcreuse.h
+ * \brief Bibliothèque de matrices creuses
+ * \author Groupe G1
+ * \version 0.1
+ * \date 27 april 2016
+ *
+ * Bibliothèque de création de matrices creuses.
+ *
+ */
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+class matcreuse{
+private:
+	vector<int> Valeurs;
+	vector<int> Lignes;
+	vector<int> Colonnes;
+public :
+	int nbLignes;
+	int nbColonnes;
+	matcreuse();
+	matcreuse(vector<int>,vector<int>,vector<int>);
+	void Add(int,int,int);
+	int indice(int,int) const;
+	bool Get(int,int,int&);
+	void ReadFile(string);
+	void WriteFile(string);
+	void Afficher();
+
+	matcreuse& operator*=(const matcreuse& b)
+	{
+		if(nbLignes != b.nbColonnes)
+		{
+			cout << "Erreur dans la multiplication de matrices. Euh.. Non" << endl;
+			return *this;
+		}
+		for(int i=0;i<nbColonnes/*+1*/;i++) // Pour chaque ligne/colonne
+		{
+			int x,y;
+			int val =0;
+			for(int j=0;j<nbLignes/*+1*/;j++)
+			{
+				if(Get(j,i,x))
+				{
+					if(b.indice(i,j)+1)
+					{
+						y = b.Valeurs.at(b.indice(i,j));
+						val += x*y;
+					}
+				}
+			}			
+			Valeurs[i] = val;
+		}
+		return *this;
+	}
+
+friend matcreuse operator*(matcreuse a, const matcreuse& b)
+{
+	a*=b;
+	return a;
+}
+
+};
+
+inline vector<string> Split(string s,char c)
+{
+    vector<string> internal;
+    stringstream ss(s);
+    string tok;
+    while(getline(ss,tok,c))
+    {
+        internal.push_back(tok);
+    }
+    return internal;
+}
